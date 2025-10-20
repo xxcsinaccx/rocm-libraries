0b90d5c065c87c2be805e24f3aa84faab56c4f1f
xxcsinaccx <xuchao12121@sina.com>
2025-10-20 14:18:58 +0800

    add unit test

--- test_correctness.py
+++ test_correctness.py
@@ -1,7 +1,7 @@
 import subprocess
 
 commands = [
-    "ROCM_PATH=/opt/rocm CXX=hipcc cmake -DBUILD_BENCHMARK=ON -DAMDGPU_TARGETS=gfx942 ../.",
+    "ROCM_PATH=/opt/rocm CXX=hipcc cmake -DBUILD_BENCHMARK=ON -DBUILD_TEST=ON -DAMDGPU_TARGETS=gfx942 ../.",
     "make -j benchmark_device_merge_sort",
     "make install",
     "./test/rocprim/test_device_merge_sort"
